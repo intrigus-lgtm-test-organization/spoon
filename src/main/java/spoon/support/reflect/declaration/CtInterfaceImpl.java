fa0b2062f5788666ba3ef3b5833db518e059f048
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2021-07-11 20:01:46 +0200

    fix: Make nested types in interfaces implicitly public static
    Interface member types should be public static, even if they are not
    manually given those modifiers. This patch automatically adds and
    removes the public and static modifiers from types that are added or
    removed from interfaces.

--- src/main/java/spoon/support/reflect/declaration/CtInterfaceImpl.java
+++ src/main/java/spoon/support/reflect/declaration/CtInterfaceImpl.java
@@ -7,8 +7,11 @@
  */
 package spoon.support.reflect.declaration;
 
+import java.util.HashSet;
+import java.util.stream.Collectors;
 import spoon.reflect.declaration.CtInterface;
 import spoon.reflect.declaration.CtType;
+import spoon.reflect.declaration.ModifierKind;
 import spoon.reflect.reference.CtExecutableReference;
 import spoon.reflect.reference.CtTypeReference;
 import spoon.reflect.visitor.CtVisitor;
@@ -19,6 +22,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+import spoon.support.reflect.CtExtendedModifier;
 
 public class CtInterfaceImpl<T> extends CtTypeImpl<T> implements CtInterface<T> {
 	private static final long serialVersionUID = 1L;
@@ -54,6 +58,46 @@ public class CtInterfaceImpl<T> extends CtTypeImpl<T> implements CtInterface<T> 
 	@Override
 	public CtInterface<T> clone() {
 		return (CtInterface<T>) super.clone();
+	}
+
+	@Override
+	public <N, C extends CtType<T>> C addNestedType(CtType<N> nestedType) {
+		super.addNestedType(nestedType);
+
+		// Type members of interfaces are implicitly public static. We need to add the implicit
+		// modifiers if they aren't public static already.
+		Set<CtExtendedModifier> modifiers = new HashSet<>(nestedType.getExtendedModifiers());
+		if (!nestedType.isPublic()) {
+			modifiers.add(new CtExtendedModifier(ModifierKind.PUBLIC, true));
+		}
+		if (!nestedType.isStatic()) {
+			modifiers.add(new CtExtendedModifier(ModifierKind.STATIC, true));
+		}
+		nestedType.setExtendedModifiers(modifiers);
+
+		return (C) this;
+	}
+
+	@Override
+	public <N> boolean removeNestedType(CtType<N> nestedType) {
+		if (!super.removeNestedType(nestedType)) {
+			return false;
+		}
+
+		// We might have added implicit public static modifiers so we need to remove them again
+		Set<CtExtendedModifier> newModifiers = nestedType.getExtendedModifiers().stream()
+				.filter(modifier -> {
+					if (!modifier.isImplicit()) {
+						return true;
+					}
+					return modifier.getKind() != ModifierKind.STATIC
+							&& modifier.getKind() != ModifierKind.PUBLIC;
+				})
+				.collect(Collectors.toCollection(HashSet::new));
+
+		nestedType.setExtendedModifiers(newModifiers);
+
+		return true;
 	}
 
 	@Override
